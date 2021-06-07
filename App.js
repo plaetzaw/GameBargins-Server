7ae2f54523d105dfd37ad91a2cc0f6771ff687f9
plaetzaw <plaetzaw@gmail.com>
2021-06-07 17:33:18 -0500

    What's up with heroku

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const PORT = process.env.PORT || 8080;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 8080
 
-require("dotenv").config();
+require('dotenv').config()
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//JWT
-const { authUser } = require("./utility/auth");
+// JWT
+const { authUser } = require('./utility/auth')
 
-//Routes
-app.use(require("./routes/register"));
-app.use(require("./routes/login", authUser));
-app.use(require("./routes/createGameEntry", authUser));
-app.use(require("./routes/searchTitle", authUser));
-app.use(require("./routes/setAlert", authUser));
-app.use(require("./routes/viewSavedGames", authUser));
-app.use(require("./routes/deleteFavorite", authUser));
-app.use(require("./routes/userSettings", authUser));
-app.use(require("./routes/userInfo", authUser));
-app.use(require("./routes/advancedSearch", authUser));
+// Routes
+app.use(require('./routes/register'))
+app.use(require('./routes/login', authUser))
+app.use(require('./routes/createGameEntry', authUser))
+app.use(require('./routes/searchTitle', authUser))
+app.use(require('./routes/setAlert', authUser))
+app.use(require('./routes/viewSavedGames', authUser))
+app.use(require('./routes/deleteFavorite', authUser))
+app.use(require('./routes/userSettings', authUser))
+app.use(require('./routes/userInfo', authUser))
+app.use(require('./routes/advancedSearch', authUser))
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}`)
+})
