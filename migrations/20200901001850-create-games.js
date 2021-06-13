7e2beb92bebd9e0865e79af8dd4c1a6a188b9877
plaetzaw <plaetzaw@gmail.com>
2021-06-13 15:22:56 -0500

    Added userID to migration

--- migrations/20200901001850-create-games.js
+++ migrations/20200901001850-create-games.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('games', {
@@ -62,6 +62,9 @@ module.exports = {
       thumb: {
         type: Sequelize.STRING
       },
+      userID: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -70,9 +73,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('games');
+    return queryInterface.dropTable('games')
   }
-};
+}
