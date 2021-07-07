9e35b70f01ffb626c369454470f6302e618a72cc
plaetzaw <plaetzaw@gmail.com>
2021-07-07 15:11:08 -0500

    added secure option to cookie

--- routes/User.js
+++ routes/User.js
@@ -53,7 +53,7 @@ router.post('/register', async (req, res) => {
     const accesstoken = generateAccessToken(user)
 
     // These will need to be set to true for production
-    const accesscookieOptions = { expiresIn: cookieExpires, httpOnly: true, useHttps: true, sameSite: 'None' }
+    const accesscookieOptions = { expiresIn: cookieExpires, httpOnly: true, useHttps: true, sameSite: 'None', secure: true }
     console.log(accesstoken)
 
     res
@@ -102,7 +102,7 @@ router.post('/login', async (req, res) => {
         // const refreshtoken = jwt.sign(user, process.env.REFRESH_SECRET)
 
         // These will need to be set to true for production
-        const accesscookieOptions = { expiresIn: cookieExpires, httpOnly: true, useHttps: true, sameSite: 'None' }
+        const accesscookieOptions = { expiresIn: cookieExpires, httpOnly: true, useHttps: true, sameSite: 'None', secure: true }
         // const refreshcookieOptions = { expiresIn: cookieExpires, httpOnly: false, useHttps: false }
 
         // refreshTokens.push(refreshtoken)
